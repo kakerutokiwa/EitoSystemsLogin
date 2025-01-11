af507280ba22193dcfca84cc269928c56e41f196
kakerutokiwa <105093890+kakerutokiwa@users.noreply.github.com>
2025-01-11 23:19:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,18 @@ document.getElementById('loginBtn').addEventListener('click', function() {
       loginButton.classList.add('done');
     }, 5000);
   }
+  // チェックボックスとPassword欄の関連付け
+const encryptionDisplayCheckbox = document.querySelector('#encryptionDisplayCheckbox');  // チェックボックス
+const passwordField = document.querySelector('#password');  // パスワード入力欄
+
+// チェックボックスの状態を監視
+encryptionDisplayCheckbox.addEventListener('change', function() {
+  if (encryptionDisplayCheckbox.checked) {
+    // チェックボックスにチェックが入った場合、パスワードを表示
+    passwordField.type = 'text';  // パスワードの内容を表示
+  } else {
+    // チェックが外れた場合、パスワードを隠す
+    passwordField.type = 'password';  // パスワードを隠す
+  }
+});
 });
